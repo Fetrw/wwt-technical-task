223bc725e675838e5e0b0c9bbe0e59223389b130
Fetrw <21vladmel15@gmail.com>
2024-10-25 22:19:00 +0300

    feat(types): add search request filter types

--- src/api/types/SearchRequest/SearchRequestFilter.ts
+++ src/api/types/SearchRequest/SearchRequestFilter.ts
@@ -2,12 +2,21 @@ import { FilterType } from '../Filter'
 
 export interface SearchRequestFilterBase {
 	id: string
+	name: string
 	type: FilterType
+}
+
+export interface FilterOption {
+	id: string
+	name: string
 }
 
 export interface SearchRequestOptions extends SearchRequestFilterBase {
 	type: FilterType.OPTION
-	optionsIds: string[]
+	options: FilterOption[]
 }
 
-export type SearchRequestFilter = SearchRequestOptions[]
+export type SearchRequestFilter = (
+	| SearchRequestOptions
+	| SearchRequestFilterBase
+)[]
